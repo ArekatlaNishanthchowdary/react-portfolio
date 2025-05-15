0e4eada49cc1154ea223e2c0812382e5a82cf8e9
Arekatla Nishanth Chowdary <av.sc.u4aie23002@av.students.amrita.edu>
2025-05-15 09:45:37 +0530

    local changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Add Font Awesome and Google Fonts
-const fontAwesome = document.createElement('link');
-fontAwesome.rel = 'stylesheet';
-fontAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css';
-fontAwesome.integrity = 'sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==';
-fontAwesome.crossOrigin = 'anonymous';
-fontAwesome.referrerPolicy = 'no-referrer';
-document.head.appendChild(fontAwesome);
+// Import Font Awesome locally
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 // Add Google Fonts
 const googleFonts = document.createElement('link');
