30e0b2276ee954e03fe4f66ce1af28f641393d89
Arekatla Nishanth Chowdary <av.sc.u4aie23002@av.students.amrita.edu>
2025-05-17 11:48:43 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/react-portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
+      }
+    }
+  }
 })
